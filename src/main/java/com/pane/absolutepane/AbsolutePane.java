bccf7310f67bc6fe724a1351894d4b3bd81dfe9a
Radosław Pikul <rapikul111@gmail.com>
2023-11-18 18:57:05 +0100

    Adding docs and fixing constructors

--- src/main/java/com/pane/absolutepane/AbsolutePane.java
+++ src/main/java/com/pane/absolutepane/AbsolutePane.java
@@ -10,68 +10,43 @@ import java.util.Map;
 public class AbsolutePane extends Pane {
     private final Map<Node, Positon> nodes;
 
+    /**
+     * Creates a new instance of AbsolutePane
+     */
     public AbsolutePane() {
         super();
-        this.nodes = new HashMap<>();
+        this.nodes = Collections.synchronizedMap(new HashMap<>());
     }
 
+    /**
+     * Creates a new instance of AbsolutePane with initial nodes
+     * @param nodes - a map of nodes and their positions
+     */
     public AbsolutePane(Map<Node,Positon> nodes) {
         super();
-        this.nodes = nodes;
+        this.nodes = Collections.synchronizedMap(nodes);
     }
 
-    public AbsolutePane(double width, double height) {
-        super();
+
+    /**
+     * Creates a new instance of AbsolutePane with specified preferred size
+     * @param width - the width of the pane
+     * @param height - the height of the pane
+     */
+    public AbsolutePane(double width, double height){
+        this();
         setPrefSize(width, height);
-        nodes = new HashMap<>();
     }
 
+    /**
+     * Creates a new instance of AbsolutePane with specified preferred size and initial nodes
+     * @param width - the width of the pane
+     * @param height - the height of the pane
+     * @param nodes - a map of nodes and their positions
+     */
     public AbsolutePane(double width, double height, Map<Node,Positon> nodes){
-        super();
+        this(nodes);
         setPrefSize(width, height);
-        this.nodes = nodes;
-    }
-
-    public AbsolutePane(boolean synchronous) {
-        super();
-        if(!synchronous) {
-            this.nodes = new HashMap<>();
-        }
-        else{
-            this.nodes = Collections.synchronizedMap(new HashMap<>());
-        }
-    }
-
-    public AbsolutePane(Map<Node,Positon> nodes,boolean synchronous) {
-        super();
-        if(!synchronous) {
-            this.nodes = new HashMap<>(nodes);
-        }
-        else{
-            this.nodes = Collections.synchronizedMap(new HashMap<>(nodes));
-        }
-    }
-
-    public AbsolutePane(double width, double height,boolean synchronous){
-        super();
-        setPrefSize(width, height);
-        if(!synchronous) {
-            this.nodes = new HashMap<>();
-        }
-        else{
-            this.nodes = Collections.synchronizedMap(new HashMap<>());
-        }
-    }
-
-    public AbsolutePane(double width, double height, Map<Node,Positon> nodes, boolean synchronous){
-        super();
-        setPrefSize(width, height);
-        if(!synchronous) {
-            this.nodes = new HashMap<>(nodes);
-        }
-        else{
-            this.nodes = Collections.synchronizedMap(new HashMap<>(nodes));
-        }
     }
 
     @Override
@@ -84,17 +59,53 @@ public class AbsolutePane extends Pane {
         }
     }
 
+    /**
+     * Adds a node to the pane
+     * @param node - the node to be added
+     * @param position - the position of the node
+     */
     public void addNode(Node node, Positon position){
         nodes.put(node, position);
         getChildren().add(node);
     }
+    /**
+     * Removes a node from the pane
+     * @param node - the node to be removed
+     */
     public void removeNode(Node node){
         nodes.remove(node);
         getChildren().remove(node);
     }
 
+    /**
+     * Adds nodes to the pane
+     * @param nodes - a map of nodes and their positions
+     */
     public void addNodes(Map<Node,Positon> nodes){
         this.nodes.putAll(nodes);
         getChildren().addAll(nodes.keySet());
+    }
+
+    /**
+     * Get node at specified position
+     * @param position - the position of the node
+     * @return {@link  javafx.scene.Node}
+     */
+    public Node getNodeAtPosition(Positon position){
+        for (Node node : nodes.keySet()) {
+            Positon nodePosition = nodes.get(node);
+            if(nodePosition.equals(position)){
+                return node;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get all nodes with their respective positions
+     * @return {@link  java.util.Map}
+     */
+    public Map<Node, Positon> getNodes() {
+        return nodes;
     }
 }
--- src/main/java/com/pane/absolutepane/Positon.java
+++ src/main/java/com/pane/absolutepane/Positon.java
@@ -3,6 +3,11 @@ package com.pane.absolutepane;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
+import java.util.Objects;
+
+/**
+ * A class that represents a position of a node in the pane
+ */
 public class Positon {
     private DoubleProperty x;
     private DoubleProperty y;
@@ -32,5 +37,18 @@ public class Positon {
     }
     public void setY(DoubleProperty y) {
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Positon positon = (Positon) o;
+        return Objects.equals(x, positon.x) && Objects.equals(y, positon.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
